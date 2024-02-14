478d9bc995eab2f169523c16a4199e4db8068c75
Henrik A. Glass <henrik.asson@gmail.com>
2024-02-14 17:01:02 +0100

    Update hgl_arena_alloc.h

--- hgl_arena_alloc.h
+++ hgl_arena_alloc.h
@@ -35,21 +35,21 @@
  *
  * Include hgl_arena_alloc.h file like this:
  *
- *     #define HGL_ARENA_ALLOC_IMPLEMENTATION 
+ *     #define HGL_ARENA_ALLOC_IMPLEMENTATION
  *     #include "hgl_arena_alloc.h"
  *
  * This will create the implementation of hgl_arena_alloc in the current compilation unit. To
- * include hgl_arena_alloc.h without creating the implementation, simply ommit the #define 
- * of HGL_ARENA_ALLOC_IMPLEMENTATION. 
+ * include hgl_arena_alloc.h without creating the implementation, simply ommit the #define
+ * of HGL_ARENA_ALLOC_IMPLEMENTATION.
  *
  * Below is a complet listing of the API:
  *
- * HglArena hgl_make_arena(size_t arena_size)
+ * HglArena hgl_arena_make(size_t arena_size)
  * void *hgl_arena_alloc(HglArena *arena, size_t alloc_size)
  * void hgl_arena_free_all(HglArena *arena)
- * void hgl_destroy_arena(HglArena *arena)
+ * void hgl_arena_destroy(HglArena *arena)
  *
- * hgl_arena_alloc allows the user to define the alignment of allocations from an arena by 
+ * hgl_arena_alloc allows the user to define the alignment of allocations from an arena by
  * redefining HGL_ARENA_ALIGNMENT, as such:
  *
  *     #define HGL_ARENA_ALIGNMENT 64
@@ -83,7 +83,7 @@
 
 typedef struct
 {
-    uint8_t *memory; 
+    uint8_t *memory;
     size_t head;
     size_t size;
 } HglArena;
@@ -91,7 +91,12 @@ typedef struct
 /**
  * Create an arena of size `arena_size`.
  */
-HglArena hgl_make_arena(size_t arena_size);
+HglArena hgl_arena_make(size_t arena_size);
+
+/**
+ * Create an arena of `buf_size` from a preallocated buffer `buf`.
+ */
+HglArena hgl_arena_make_from_buffer(void *buf, size_t buf_size);
 
 /**
  * Allocate a chunk of `alloc_size` bytes from `arena`.
@@ -106,7 +111,7 @@ void hgl_arena_free_all(HglArena *arena);
 /**
  * Destroy the arena (Free the arena itself).
  */
-void hgl_destroy_arena(HglArena *arena);
+void hgl_arena_destroy(HglArena *arena);
 
 #endif /* HGL_ARENA_ALLOC_H */
 
@@ -116,7 +121,7 @@ void hgl_destroy_arena(HglArena *arena);
 
 #ifdef HGL_ARENA_ALLOC_IMPLEMENTATION
 
-HglArena hgl_make_arena(size_t arena_size)
+HglArena hgl_arena_make(size_t arena_size)
 {
     static_assert(HGL_ARENA_ALIGNMENT % 2 == 0);
     void *mem = aligned_alloc(HGL_ARENA_ALIGNMENT, arena_size);
@@ -125,6 +130,15 @@ HglArena hgl_make_arena(size_t arena_size)
         .memory = mem,
         .head = 0,
         .size = (mem == NULL) ? 0 : arena_size,
+    };
+}
+
+HglArena hgl_arena_make_from_buffer(void *buf, size_t buf_size)
+{
+    return (HglArena) {
+        .memory = (uint8_t *) buf,
+        .head = 0,
+        .size = buf_size,
     };
 }
 
@@ -147,10 +161,10 @@ void *hgl_arena_alloc(HglArena *arena, size_t alloc_size)
     }
 
     /* Move head to nearest multiple of alignment after `head` + `alloc_size` */
-    arena->head += (alloc_size + HGL_ARENA_ALIGNMENT - 1) & 
+    arena->head += (alloc_size + HGL_ARENA_ALIGNMENT - 1) &
                   ~(HGL_ARENA_ALIGNMENT - 1);
 #ifdef HGL_ARENA_ALLOC_DEBUG_PRINTS
-    printf("Arena usage: %f%% of %lu KiB\n", 
+    printf("Arena usage: %f%% of %lu KiB\n",
            100.f * ((float) arena->head / (float) arena->size),
            (arena->size / 1024));
 #endif /* HGL_ARENA_ALLOC_DEBUG_PRINTS */
@@ -162,10 +176,9 @@ void hgl_arena_free_all(HglArena *arena)
     arena->head = 0;
 }
 
-void hgl_destroy_arena(HglArena *arena)
+void hgl_arena_destroy(HglArena *arena)
 {
-    free(arena->memory);
+    (free)(arena->memory);
 }
 
 #endif /* HGL_ARENA_ALLOC_IMPLEMENTATION */
-
