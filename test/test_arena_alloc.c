521f742adc7113b94a9b10a23a34dbed2e3e84ea
Henrik A. Glass <henrik.asson@gmail.com>
2024-02-14 17:05:19 +0100

    Update test_arena_alloc.c

--- test/test_arena_alloc.c
+++ test/test_arena_alloc.c
@@ -4,6 +4,8 @@
 #define HGL_ARENA_ALLOC_IMPLEMENTATION
 #include "hgl_arena_alloc.h"
 
+static uint8_t memory_chunk[128 * 1024];
+
 static HglArena s_arena;
 
 void *arena_alloc(size_t size)
@@ -13,8 +15,9 @@ void *arena_alloc(size_t size)
 
 int main()
 {
-    s_arena = hgl_make_arena(128 * 1024);  // 128 KiB
-    //s_arena = hgl_make_arena(1024);  // 1 KiB
+    //s_arena = hgl_arena_make(128 * 1024);  // 128 KiB
+    s_arena = hgl_arena_make_from_buffer(memory_chunk, sizeof(memory_chunk));
+    //s_arena = hgl_arena_make(1024);  // 1 KiB
 
 
     void *a = arena_alloc(32);
@@ -26,7 +29,7 @@ int main()
     printf("%p\n", b);
     printf("%p\n", c);
     printf("%p\n", d);
-    
+
     printf("%08lX\n", (uint8_t*)a - s_arena.memory);
     printf("%08lX\n", (uint8_t*)b - s_arena.memory);
     printf("%08lX\n", (uint8_t*)c - s_arena.memory);
@@ -35,7 +38,8 @@ int main()
     hgl_arena_free_all(&s_arena);
 
     a = arena_alloc(128 * 1024);
+
     printf("%08lX\n", (uint8_t*)a - s_arena.memory);
 
-    hgl_destroy_arena(&s_arena);
+    //hgl_arena_destroy(&s_arena);
 }
