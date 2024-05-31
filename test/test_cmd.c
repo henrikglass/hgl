741a1904f40ce808db4b5e5d4bfb3e9322176fa8
Henrik A. Glass <henrik.asson@gmail.com>
2024-05-31 17:59:03 +0200

    Create test_cmd.c

---
+++ test/test_cmd.c
@@ -0,0 +1,77 @@
+
+typedef void (*proc_t)(void);
+
+#define HGL_CMD_PRIVATE_DATA_T proc_t
+#define HGL_CMD_IMPLEMENTATION
+#include "hgl_cmd.h"
+
+#include <stddef.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+void open_door(void);
+void open_door()
+{
+    printf("Opening door\n");
+}
+
+void operate_tractor(void);
+void operate_tractor()
+{
+    printf("Operating tractor\n");
+}
+
+void operate_bike(void);
+void operate_bike()
+{
+    printf("Operating bike. Pling pling!\n");
+}
+
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wmissing-field-initializers"
+static HglCommand command_tree[] =
+{
+    {HGL_CMD_LEAF, "hello", "prints \"Hello World!\""},
+    {HGL_CMD_LEAF, "goodbye", "prints \"Goodbye folks!\""},
+    {HGL_CMD_LEAF, "hejsan", "prints \"Hejsan Hoppsan!\""},
+    {HGL_CMD_NODE, "operate", "operates something", .sub_tree = (HglCommand[]){
+        {HGL_CMD_LEAF, "forklift", "a forklift"},
+        {HGL_CMD_NODE, "vehicle", "some vehicle", .sub_tree = (HglCommand[]){
+            {HGL_CMD_LEAF, "car", "a car"},
+            {HGL_CMD_LEAF, "bike", "a bike"},
+            {HGL_CMD_LEAF, "tractor", "a tractor", .private_data = operate_tractor},
+            {HGL_CMD_NONE}
+        }},
+        {HGL_CMD_NONE}
+    }},
+    {HGL_CMD_NODE, "open", "opens something", .sub_tree = (HglCommand[]){
+        {HGL_CMD_LEAF, "jar", "a jar"},
+        {HGL_CMD_LEAF, "can", "a can"},
+        {HGL_CMD_LEAF, "door", "a door", .private_data = open_door},
+        {HGL_CMD_NONE}
+    }},
+    {HGL_CMD_LEAF, "help", "prints help message"},
+    {HGL_CMD_NONE}
+};
+#pragma GCC diagnostic pop
+
+char buf[64];
+
+int main()
+{
+    hgl_cmd_tree_at(command_tree, "operate", "vehicle", "bike")->private_data = operate_bike;
+
+    while (true) {
+        size_t lpos;
+        const HglCommand *cmd = hgl_cmd_input(command_tree, ">>> ", buf, 64, &lpos);
+        if (hgl_cmd_has_private_data(cmd)) {
+            proc_t f = cmd->private_data;
+            f();
+        }
+        if (cmd == hgl_cmd_tree_at(command_tree, "help")) {
+            hgl_cmd_tree_print(command_tree, 2);
+        }
+        memset(buf, 0, 64);
+    }
+}
