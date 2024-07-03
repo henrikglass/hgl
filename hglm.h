9a1a5091d604689805cb2202cb037f71e3bb3443
Henrik A. Glass <henrik.asson@gmail.com>
2024-07-03 18:06:31 +0200

    Update hglm.h

--- hglm.h
+++ hglm.h
@@ -1033,7 +1033,7 @@ static HGL_INLINE void hglm_mat_transpose(HglmMat res, HglmMat m)
 }
 
 
-/* ========== scalar interpolation functions =================================*/
+/* ========== scalar & misc. math functions ==================================*/
 
 static HGL_INLINE float hglm_lerp(float a, float b, float amount)
 {
@@ -1068,6 +1068,11 @@ static HGL_INLINE float hglm_smoothstep(float t)
     return t * t * (3.0f - 2.0f * t);
 }
 
+static HGL_INLINE float hglm_smootherstep(float t)
+{
+    return t * t * t * (t * (t * 6.0f - 15.0f) + 10.0f);
+}
+
 static HGL_INLINE float hglm_sinstep(float t)
 {
     return -0.5f * cosf(t * HGLM_PI) + 0.5f;
@@ -1077,7 +1082,6 @@ static HGL_INLINE float hglm_lerpsmooth(float a, float b, float dt, float omega)
 {
     return b + (a - b) * exp2f(-dt/omega);
 }
-
 
 static HGL_INLINE HglmVec4 hglm_bezier3(float t)
 {
@@ -1092,6 +1096,61 @@ static HGL_INLINE HglmVec4 hglm_bezier3(float t)
         .z = hglm_vec4_dot(ts, bezier3.c2),
         .w = hglm_vec4_dot(ts, bezier3.c3),
     };
+}
+
+static HGL_INLINE float hglm_grad(int hash, float x, float y, float z)
+{
+    int h = hash & 15;
+    float u = (h < 8) ? x : y;
+    float v = (h < 4) ? y : ((h == 12) || (h == 14)) ? x : z;
+    return (((h & 1) == 0) ? u : -u) +
+           (((h & 2) == 0) ? v : -v);
+}
+
+static HGL_INLINE float hglm_perlin3D(float x, float y, float z)
+{
+    static const int P[512] = {
+        151, 160, 137, 91, 90, 15, 131, 13, 201, 95, 96, 53, 194, 233, 7, 225, 140,
+        36, 103, 30, 69, 142, 8, 99, 37, 240, 21, 10, 23, 190, 6, 148, 247, 120,
+        234, 75, 0, 26, 197, 62, 94, 252, 219, 203, 117, 35, 11, 32, 57, 177, 33,
+        88, 237, 149, 56, 87, 174, 20, 125, 136, 171, 168, 68, 175, 74, 165, 71,
+        134, 139, 48, 27, 166, 77, 146, 158, 231, 83, 111, 229, 122, 60, 211, 133,
+        230, 220, 105, 92, 41, 55, 46, 245, 40, 244, 102, 143, 54, 65, 25, 63, 161,
+        1, 216, 80, 73, 209, 76, 132, 187, 208, 89, 18, 169, 200, 196, 135, 130, 116,
+        188, 159, 86, 164, 100, 109, 198, 173, 186, 3, 64, 52, 217, 226, 250, 124,
+        123, 5, 202, 38, 147, 118, 126, 255, 82, 85, 212, 207, 206, 59, 227, 47, 16,
+        58, 17, 182, 189, 28, 42, 223, 183, 170, 213, 119, 248, 152, 2, 44, 154, 163,
+        70, 221, 153, 101, 155, 167, 43, 172, 9, 129, 22, 39, 253, 19, 98, 108, 110,
+        79, 113, 224, 232, 178, 185, 112, 104, 218, 246, 97, 228, 251, 34, 242, 193,
+        238, 210, 144, 12, 191, 179, 162, 241, 81, 51, 145, 235, 249, 14, 239, 107,
+        49, 192, 214, 31, 181, 199, 106, 157, 184, 84, 204, 176, 115, 121, 50, 45,
+        127, 4, 150, 254, 138, 236, 205, 93, 222, 114, 67, 29, 24, 72, 243, 141,
+        128, 195, 78, 66, 215, 61, 156, 180
+    };
+
+    int X = (int)floorf(x) & 255;
+    int Y = (int)floorf(y) & 255;
+    int Z = (int)floorf(z) & 255;
+    x -= floorf(x);
+    y -= floorf(y);
+    z -= floorf(z);
+    float u = hglm_smootherstep(x);
+    float v = hglm_smootherstep(y);
+    float w = hglm_smootherstep(z);
+    int A  = P[X] + Y;
+    int AA = P[A] + Z;
+    int AB = P[A+1] + Z;
+    int B  = P[X+1] + Y;
+    int BA = P[B] + Z;
+    int BB = P[B+1] + Z;
+    return 0.5f + hglm_lerp(hglm_lerp(hglm_lerp(hglm_grad(P[AA  ], x    , y    , z    ),
+                                                hglm_grad(P[BA  ], x - 1, y    , z    ), u),
+                                      hglm_lerp(hglm_grad(P[AB  ], x    , y - 1, z    ),
+                                                hglm_grad(P[BB  ], x - 1, y - 1, z    ), u), v),
+                            hglm_lerp(hglm_lerp(hglm_grad(P[AA+1], x    , y    , z - 1),
+                                                hglm_grad(P[BA+1], x - 1, y    , z - 1), u),
+                                      hglm_lerp(hglm_grad(P[AB+1], x    , y - 1, z - 1),
+                                                hglm_grad(P[BB+1], x - 1, y - 1, z - 1), u), v), w);
 }
 
 #endif /* HGLM_H */
